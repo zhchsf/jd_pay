aa0e51bdba60ee23fe85bae5f9d5e96b00d9103a
sam <zhchsf@gmail.com>
2016-09-26 14:02:56 +0800

    解密逻辑，未加测试

--- lib/jd_pay/util/des3.rb
+++ lib/jd_pay/util/des3.rb
@@ -1,6 +1,6 @@
 module JdPay
   module Util
-    # 京东支付 des3 加密，掺杂了京东自定义的一些位转移逻辑
+    # 京东支付 des3 加解密，掺杂了京东自定义的一些位转移逻辑
     class Des3
       def self.encrypt(source, base64_key)
         key = Base64.decode64(base64_key)
@@ -14,6 +14,21 @@ module JdPay
         res.unpack("H*").first
       end
 
+      def self.decrypt(source, base64_key)
+        key = Base64.decode64(base64_key)
+        b2 = source.chars.each_slice(2).map{|x| x.join().to_i(16) }.map(&:chr).join
+
+        des = OpenSSL::Cipher::Cipher.new('des-ede3')
+        des.decrypt
+        des.key = key
+        des.padding = 0
+        res = des.update(b2) + des.final
+        decrypt_bytes = res.bytes
+
+        bytes_length = bytes_to_int(decrypt_bytes[0, 4])
+        decrypt_bytes[4, 4 + bytes_length].map(&:chr).join
+      end
+
       # 对要加密的字符串按照京东的规则处理
       def self.transform_source(source_bytes)
         source_len = source_bytes.length
@@ -21,14 +36,23 @@ module JdPay
         y = x == 0 ? 0 : 8 - x
 
         result_bytes = []
-        result_bytes << (source_len >> 24 & 0xFF)
-        result_bytes << (source_len >> 16 & 0xFF)
-        result_bytes << (source_len >> 8 & 0xFF)
-        result_bytes << (source_len & 0xFF)
+        0.upto(3).each do |index|
+          result_bytes << ((source_len >> (3 - index) * 8) & 0xFF)
+        end
         result_bytes += source_bytes
         y.times { result_bytes << 0 }
 
         result_bytes.map(&:chr).join
+      end
+
+      # 解密
+      def self.bytes_to_int(bytes)
+        total = 0
+        bytes.each_with_index do |value, index|
+          shift = (3 - index) * 8
+          total += (value & 0xFF) << shift
+        end
+        total
       end
     end
   end
